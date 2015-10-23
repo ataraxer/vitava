696492183449a6043dee61936fa7e3d9443e5efd
Anton Karamanov <ataraxer@gmail.com>
2015-10-23 11:31:24 +0300

    Add simple message conversation over Kafka protocol

--- Main.scala
+++ Main.scala
@@ -5,7 +5,10 @@ import akka.stream.io.Framing
 import akka.util.ByteString
 
 import kafka.api._
+import kafka.common.TopicAndPartition
+import kafka.message._
 
+import scala.collection.mutable
 
 
 object Main extends StreamApp {
@@ -21,12 +24,56 @@ object Main extends StreamApp {
   val host = scala.sys.process.Process("boot2docker ip").!!.trim
   val connection = Tcp().outgoingConnection(host, 9092)
 
+  val kafka = kafkaProtocol join connection
 
-  val message = new TopicMetadataRequest(42, 9000, "foobar", Seq("topic"))
+  val correlationId = Iterator.from(0).next _
+  val clientId = "foobar"
+  val topic = "topic"
+  val partition = TopicAndPartition(topic, 0)
 
-  Source.single(message)
-    .via(kafkaProtocol join connection)
+  val metadataRequest = new TopicMetadataRequest(
+    0, correlationId(), clientId, Seq(topic))
+
+  val offsetsRequest = OffsetRequest(
+    Map(partition -> PartitionOffsetRequestInfo(OffsetRequest.LatestTime, 1)),
+    correlationId = correlationId(),
+    clientId = clientId)
+
+  val payload = ByteString("The quick brown fox jumps over the lazy dog.")
+  val key = ByteString("tag:poetry")
+
+  val data = new Message(
+    bytes = payload.toArray[Byte],
+    key = key.toArray[Byte],
+    codec = NoCompressionCodec,
+    payloadOffset = 0,
+    payloadSize = payload.size)
+
+  def fetchRequest(offset: Long) = new FetchRequest(
+    correlationId(),
+    clientId,
+    maxWait = 500,
+    minBytes = 64 * 1024,
+    requestInfo = Map(partition -> PartitionFetchInfo(offset, 64 * 1024)))
+
+  val produceRequest = new ProducerRequest(
+    correlationId(),
+    clientId = clientId,
+    requiredAcks = -1.toShort,
+    ackTimeoutMs = 8 * 1000,
+    data = mutable.Map(partition -> new ByteBufferMessageSet(data)))
+
+  val message = Vector[RequestOrResponse](
+    metadataRequest,
+    offsetsRequest,
+    produceRequest,
+    offsetsRequest,
+    fetchRequest(9))
+
+  Source(message)
+    .via(kafka)
     .recover(errorHandler)
+    .map(KafkaApi.formatResponse)
     .runForeach(println)
 }
 
